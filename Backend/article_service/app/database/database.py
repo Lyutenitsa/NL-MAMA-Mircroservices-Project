1f361a1b033cd812a30fe6ee2d3b10d44c8ec95a
lyutenitsa <lutfi.ismail5@gmail.com>
2023-01-14 02:20:38 +0100

    database url changes

--- Backend/article_service/app/database/database.py
+++ Backend/article_service/app/database/database.py
@@ -6,15 +6,15 @@ from sqlalchemy.orm import sessionmaker
 from ..config import settings
 
 if os.getenv("DATABASE_URL"):
-	SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 else:
-	print("DATABASE_URL not found")
-	SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:" \
-	                          f"{settings.database_password}" \
-	                          f"@{settings.database_host}:" \
-	                          f"{settings.database_port}" \
-	                          f"/{settings.database_name}"
+    print("DATABASE_URL not found")
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:" \
+                              f"{settings.database_password}" \
+                              f"@{settings.database_host}:" \
+                              f"{settings.database_port}" \
+                              f"/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -24,8 +24,8 @@ Base = declarative_base()
 
 
 def get_db():
-	db = SessionLocal()
-	try:
-		yield db
-	finally:
-		db.close()
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- Backend/topic_service/app/database/database.py
+++ Backend/topic_service/app/database/database.py
@@ -6,15 +6,16 @@ from sqlalchemy.orm import sessionmaker
 from ..config import settings
 
 if os.getenv("DATABASE_URL"):
-	SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 else:
-	SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:" \
-	                          f"{settings.database_password}" \
-	                          f"@{settings.database_host}:" \
-	                          f"{settings.database_port}" \
-	                          f"/{settings.database_name}"
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:" \
+                              f"{settings.database_password}" \
+                              f"@{settings.database_host}:" \
+                              f"{settings.database_port}" \
+                              f"/{settings.database_name}"
 
+print("printing database url")
 print(SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -25,8 +26,8 @@ Base = declarative_base()
 
 
 def get_db():
-	db = SessionLocal()
-	try:
-		yield db
-	finally:
-		db.close()
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
